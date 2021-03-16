dea03684a6d5c8d475c3b5ac4659b14491258e27
Senay Yakut <senaykt@gmail.com>
2021-03-15 20:53:56 -0700

    Import React and ReactDOM

---
+++ app.js
@@ -0,0 +1,7 @@
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "";
+const animalFacts = ( < h1 > title < /h1>);
+        title === '' ? '' : title;
