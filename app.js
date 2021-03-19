d58721d01884060a4b6ff4f4e4e5c15068fba0f5
Senay Yakut <senaykt@gmail.com>
2021-03-18 22:20:36 -0700

    Add ternarary operator

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ import { animals } from './animals';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = "";
-const animalFacts = ( < h1 > { title } < /h1>);
-        title === '' ? animalFacts[click] : title;
+const title = " ";
+const animalFacts = < h1 > { title === '' ? "Click an animal for fun fact" : title } < /h1>;
 
-        RenderDOM.render(animalFacts, document.getElementById("root"));
+RenderDOM.render(animalFacts, document.getElementById("root"));
