14ddc7dde23d7c88b4dfa2ee635d4acd2b27abfd
Senay Yakut <senaykt@gmail.com>
2021-03-19 21:21:46 -0700

    Add the attributes

--- app.js
+++ app.js
@@ -2,7 +2,39 @@ import { animals } from './animals';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = " ";
-const animalFacts = < h1 > { title === '' ? "Click an animal for fun fact" : title } < /h1>;
+const images = [];
+for (const animal in animals) {
+    images.push( <
+        img key = { animal }
+        className = 'animal'
+        alt = { animal }
+        src = { animals[animal].image }
+        aria - label = { animal }
+        role = 'button'
 
-RenderDOM.render(animalFacts, document.getElementById("root"));
+        /
+        >
+    )
+};
+const title = "";
+const background = ( <
+    img className = "background"
+    alt = "ocean"
+    src = "images/ocean.jpg" / >
+);
+const animalFacts = ( <
+    div >
+    <
+    h1 > { title === "" ? "Click an animal for fun fact" : title } <
+    /h1> { background } <
+    div className = "animals" > { images } <
+    /div> <
+    /div>
+);
+
+const optionIndex = Math.floor(Math.random() * selectedAnimal.facts.length);
+
+function displayFact(e) {
+
+}
+ReactDOM.render(animalFacts, document.getElementById("root"));
