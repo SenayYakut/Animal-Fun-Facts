fffd186d4be47c93226f4fc0ae2df4e4675d98e7
Senay Yakut <senaykt@gmail.com>
2021-07-12 20:00:36 -0700

    Add more feature to the project

--- app.js
+++ app.js
@@ -2,45 +2,49 @@ import { animals } from './animals';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const images = [];
-for (const animal in animals) {
-    images.push( <
-        img key = { animal }
-        className = 'animal'
-        alt = { animal }
-        src = { animals[animal].image }
-        aria - label = { animal }
-        role = 'button'
-
-        /
-        >
-    )
-};
-const title = "";
-const background = ( <
-    img className = "background"
+const title = '';
+const background = (
+  <img 
+    className="background"
     alt = "ocean"
-    src = "images/ocean.jpg" /
-    >
-);
-const animalFacts = ( <
-    div >
-    <
-    h1 > { title === "" ? "Click an animal for fun fact" : title } <
-    /h1> { background } <
-    p id = "fact" > < /p> <
-    div className = "animals" > { images } <
-    /div> < /
-    div >
+    src = '/images/ocean.jpg' />
 );
 
+const images = [];
+for(const animal in animals){
+  images.push(
+    <img
+      key={animal}
+      className='animal'
+      alt= {animal}
+      src= {animals[animal].image}
+      aria-label= {animal}
+      role= 'button'
+      onClick={displayFacts}/>
+  )
+};
+
+const showBackground = true;
+
+const animalFacts = (
+  <div>
+    <h1>{title === "" ? 'Click an animal for a fun fact' : title} </h1>
+    {showBackground === true && background}
+    <p id="fact"></p>
+    <div className="animals">{images}</div>
+  </div>
+);
+
+function displayFacts(e){
+  const selectedAnimal = e.target.alt;
+  const animalInfo = animals[selectedAnimal];
+  const randomAnimalFactInd = Math.floor(Math.random() * animalInfo.facts.length);
+  const funFact = animalInfo.facts[randomAnimalFactInd];
+  document.getElementById("fact").innerHTML = funFact;
+};
 
 
-function displayFact(e) {
-    const selectedAnimal = e.target.alt;
-    const animalInfo = animals[selectedAnimal];
-    const optionIndex = Math.floor(Math.random() * selectedAnimal.facts.length);
-    const funFact = animalInfo.facts[optionalIndex];
-    document.getElementById('fact').innerHTML = funFact;
-}
-ReactDOM.render(animalFacts, document.getElementById("root"));
+ReactDOM.render(
+  animalFacts,
+  document.getElementById("root")
+);
